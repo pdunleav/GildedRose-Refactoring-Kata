17ce630e4730087a849165caba5e7343cc03609e
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2017-03-19 20:44:46 +0000

    first two passing specs - refactorying will be needed later

--- ruby-gilded-rose/gilded_rose_spec.rb
+++ ruby-gilded-rose/gilded_rose_spec.rb
@@ -3,11 +3,21 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe "AgedBrie" do
-    it "increases in quality as time passes"
+    it "increases in quality as time passes" do
+      item = Item.new("Aged Brie", 10, 10)
+      gr = GildedRose.new([item])
+      gr.update_quality
+      expect(item.quality).to be(11)
+    end
   end
 
   describe "Sulfuras" do
-    it "never has to be sold"
+    it "never has to be sold" do
+      item = Item.new("Sulfuras, Hand of Ragnaros", 10, 80)
+      gr = GildedRose.new([item])
+      gr.update_quality
+      expect(item.sell_in).to be(10)
+    end
 
     it "does not decrease in quality"
 
