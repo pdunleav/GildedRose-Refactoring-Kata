6e0752aebeab020cc5aaa767eb79d44d51cb22aa
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2017-03-20 00:33:30 +0000

    refactored specs and added failing specs for conjured items (not yet implemented

--- ruby-gilded-rose/gilded_rose_spec.rb
+++ ruby-gilded-rose/gilded_rose_spec.rb
@@ -26,13 +26,37 @@ describe GildedRose do
     end
   end
 
+  describe "Conjured Item" do
+    let(:item) { Item.new("Conjured", 8, 20) }
+    let(:gr) { GildedRose.new([item]) }
+
+    describe "decreases in quality twice as fast as regular items" do
+      it "decreases in quality as time passes" do
+      expect{gr.update_quality}.to change{item.quality}.by(-2)
+    end
+
+    it "never has a quality less than 0" do
+      50.times do
+        gr.update_quality
+      end
+      expect(item.quality).to eq(0)
+    end
+
+    context "when the sell by date has passed" do
+      it "decreases in quality twice as fast" do
+        item.sell_in = 0
+        expect{gr.update_quality}.to change{item.quality}.by(-4)
+      end
+    end
+    end
+  end
+
   describe "AgedBrie" do
     let(:item) { Item.new("Aged Brie", 10, 10) }
     let(:gr) { GildedRose.new([item]) }
 
     it "increases in quality as time passes" do
-      gr.update_quality
-      expect(item.quality).to be(11)
+      expect{gr.update_quality}.to change{item.quality}.by(1)
     end
 
     it "never has a quality greater than 50" do
@@ -48,8 +72,7 @@ describe GildedRose do
     let(:gr) { GildedRose.new([item]) }
 
     it "never has to be sold" do
-      gr.update_quality
-      expect(item.sell_in).to be(10)
+      expect{gr.update_quality}.to change{item.sell_in}.by(0)
     end
 
     it "does not decrease in quality" do
@@ -65,8 +88,7 @@ describe GildedRose do
     let(:gr) { GildedRose.new([item]) }
 
     it "increases in quality as time passes" do
-      gr.update_quality
-      expect(item.quality).to be > 10
+      expect{gr.update_quality}.to change{item.quality}.by(1)
     end
 
     it "has no quality after the concert" do
