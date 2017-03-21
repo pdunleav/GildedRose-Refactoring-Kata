d544b2787e57ec2f41b8dff334f37076638d4ced
Phelim Dunleavy <pdunleavy@bookingbug.com>
2017-03-21 13:02:42 +0000

    simplified the code

--- ruby-gilded-rose/lib/gilded_rose.rb
+++ ruby-gilded-rose/lib/gilded_rose.rb
@@ -6,30 +6,28 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert"
+      if item.name == "Aged Brie" 
         increase_quality_of(item)
-      else
-        decrease_quality_of(item)
-      end
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        increase_quality_of(item)
         if item.sell_in < 11
           increase_quality_of(item)
         end
         if item.sell_in < 6
           increase_quality_of(item)
         end
+      else
+        decrease_quality_of(item)
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in -= 1
       end
-      if item.sell_in < 0
-        if item.name == "Aged Brie"
-          increase_quality_of(item)
-        elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-          item.quality = 0
-        else
-          decrease_quality_of(item)
-        end
+      if item.name == "Aged Brie"
+        increase_quality_of(item) if passed_sell_by?(item)
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        item.quality = 0 if passed_sell_by?(item)
+      else
+        decrease_quality_of(item) if passed_sell_by?(item)
       end
     end
   end
@@ -47,7 +45,7 @@ class GildedRose
   end
 
   def passed_sell_by?(item)
-    item.sell_on < 0
+    item.sell_in < 0
   end
 end
 
